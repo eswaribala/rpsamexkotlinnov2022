0e940daee671bf25caeecc662bc604f66100b1d3
eswaribala <parameswaribala@gmail.com>
2022-11-14 13:18:50 +0530

    updated

---
+++ bankingapp/src/main/kotlin/com/amex/bankingapp/utility/Array.kt
@@ -0,0 +1,10 @@
+package com.amex.bankingapp.utility
+
+fun main(){
+    var customerIds= arrayOf(49394,3496594,347534750);
+    customerIds= arrayOf(35697,57070,350570)
+
+    for( id in customerIds){
+        println(id)
+    }
+}
