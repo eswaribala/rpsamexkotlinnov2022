16d24ed5651bc08818abfa1f4157a84d239e220a
eswaribala <parameswaribala@gmail.com>
2022-11-14 15:10:31 +0530

    updated

--- bankingapp/src/main/kotlin/com/amex/bankingapp/utility/OTPGen.kt
+++ bankingapp/src/main/kotlin/com/amex/bankingapp/utility/OTPGen.kt
@@ -2,10 +2,17 @@ package com.amex.bankingapp.utility
 
 import kotlin.random.Random
 //type is inferred
-fun otpgen(name:String, id:Int)=name+id+ Random.nextInt(1000)
+//parameter
+//conditional statement
+fun otpgen(name:String, id:Int)= if (id>10000) name.substring(2,4)+id+ Random.nextInt(1000)
+           else name.substring(6,8)+id+ Random.nextInt(5000)
 
 fun main(){
    var result = otpgen("Parameswari",6725467)
    println(result)
    println(result::class.java.typeName)
+
+   var orgName="American Express"
+
+   println("Coining the Organization Name,${orgName.substring(0,8)}")
 }
