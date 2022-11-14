60c07efb2e1aaafdc0f606d016aa96b5637fd78f
eswaribala <parameswaribala@gmail.com>
2022-11-14 14:27:07 +0530

    updated

--- bankingapp/src/main/kotlin/com/amex/bankingapp/utility/Array.kt
+++ bankingapp/src/main/kotlin/com/amex/bankingapp/utility/Array.kt
@@ -1,10 +1,20 @@
 package com.amex.bankingapp.utility
 
-fun main(){
-    var customerIds= arrayOf(49394,3496594,347534750);
-    customerIds= arrayOf(35697,57070,350570)
+import java.util.Random
 
+fun main(){
+    //var customerIds= arrayOf(49394,3496594,347534750);
+   // customerIds= arrayOf()
+    val customerIds = arrayOfNulls<Number>(5)
+
+    for (id in customerIds.indices) {
+        customerIds[id] = Random().nextInt(1000)
+    }
+/*
     for( id in customerIds){
         println(id)
     }
+
+ */
+    customerIds.forEach { id -> println(id) }
 }
