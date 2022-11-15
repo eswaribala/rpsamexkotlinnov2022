22739a4011693b920d45c12c9fc0ecd4f40a23ad
eswaribala <parameswaribala@gmail.com>
2022-11-15 11:10:05 +0530

    updated

--- bankingapp/src/main/kotlin/com/amex/bankingapp/models/CurrentAccount.kt
+++ bankingapp/src/main/kotlin/com/amex/bankingapp/models/CurrentAccount.kt
@@ -2,8 +2,8 @@ package com.amex.bankingapp.models
 
 import java.time.LocalDate
 
-class CurrentAccount(override var runningTotal: Long, override var openingDate: LocalDate) :Account() {
+class CurrentAccount(override var runningTotal: Long, override var openingDate: LocalDate,od: Long) :Account() {
     override fun showCustomerTransactions() {
-        
+
     }
 }
--- bankingapp/src/main/kotlin/com/amex/bankingapp/models/SavingsAccount.kt
+++ bankingapp/src/main/kotlin/com/amex/bankingapp/models/SavingsAccount.kt
@@ -2,7 +2,7 @@ package com.amex.bankingapp.models
 
 import java.time.LocalDate
 
-class SavingsAccount(override var runningTotal: Long, override var openingDate: LocalDate) : Account() {
+class SavingsAccount(override var runningTotal: Long, override var openingDate: LocalDate,roi: Float) : Account() {
     override fun showCustomerTransactions() {
 
     }
