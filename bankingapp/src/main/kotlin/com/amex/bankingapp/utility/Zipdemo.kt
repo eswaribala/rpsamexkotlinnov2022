25373a117245c8cdecb3111ec8f2ea874d272974
eswaribala <parameswaribala@gmail.com>
2022-11-16 10:45:39 +0530

    updated

--- bankingapp/src/main/kotlin/com/amex/bankingapp/utility/Zipdemo.kt
+++ bankingapp/src/main/kotlin/com/amex/bankingapp/utility/Zipdemo.kt
@@ -4,8 +4,11 @@ fun main(){
 
     var companyList= listOf<String>("TCS","Infosys","Accenture","Amex","HCL")
     var shareIndexList= listOf<Int>(2500,2550,1800,2800)
+    var sharemap= mutableMapOf<String,Int>()
 
     //tcs->2500, infosys->2550, accenture->1800, amex->2800
-
+    companyList.zip(shareIndexList).forEach{(i1,i2)->sharemap.put(i1,i2)}
     companyList.zip(shareIndexList).forEach{(i1,i2)->println("$i1,$i2")}
+
+    println(sharemap.values.forEach{it->println("$it")})
 }
