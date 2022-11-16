3e326de7fcf3d94055906a4fb06837dd3266b0d8
eswaribala <parameswaribala@gmail.com>
2022-11-16 17:11:20 +0530

    updated

---
+++ bankingapp/src/main/kotlin/com/amex/bankingapp/coroutines/ChannelConflatedDemo.kt
@@ -0,0 +1,36 @@
+package com.amex.bankingapp.coroutines
+
+import com.amex.bankingapp.dao.IndividualImpl
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.random.Random
+
+suspend fun main(): Unit= coroutineScope {
+//channel
+    var channel = Channel<Long>(capacity = Channel.CONFLATED)
+    var individualImpl=IndividualImpl()
+
+
+    launch {
+
+        individualImpl.getAllIndividuals().forEach{
+            it->
+            channel.send(it.accountNo)
+            delay(1000)
+        }
+        channel.close()
+    }
+
+    launch {
+        //channel iterator
+        for (x in channel)
+
+            println(channel.receive())
+
+    }
+
+}
---
+++ bankingapp/src/main/kotlin/com/amex/bankingapp/coroutines/FanoutChannel.kt
@@ -0,0 +1,32 @@
+package com.amex.coroutines
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+fun CoroutineScope.produceNumbers() = produce {
+    repeat(10) {
+        delay(100)
+        send(it)
+    }
+}
+
+fun CoroutineScope.launchProcessor(
+    id: Int,
+    channel: ReceiveChannel<Int>
+) = launch {
+    for (msg in channel) {
+        println("#$id received $msg")
+    }
+}
+
+suspend fun main(): Unit = coroutineScope {
+    val channel = produceNumbers()
+    repeat(3) { id ->
+        delay(10)
+        launchProcessor(id, channel)
+    }
+}
