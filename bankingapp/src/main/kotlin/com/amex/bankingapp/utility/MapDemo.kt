c4ef9f0a42d99af9c413f91cf5deedc0789c558e
eswaribala <parameswaribala@gmail.com>
2022-11-16 10:29:09 +0530

    updated

--- bankingapp/src/main/kotlin/com/amex/bankingapp/utility/MapDemo.kt
+++ bankingapp/src/main/kotlin/com/amex/bankingapp/utility/MapDemo.kt
@@ -10,12 +10,18 @@ fun main(){
 
     var individualsMap= mutableMapOf<Int,Individual>()
 
-    for(value in 1..100){
+    for(value in 1..25){
         individualsMap.put(Random.nextInt(1000),Individual(Random.nextInt(10000).toLong(),"Parameswari"+value, Address("","","",
             "",0),995203286+Random.nextInt(9).toLong(),"Parameswairbala@gmail.com","Test@123","Bala",
             Gender.FEMALE,             LocalDate.of(1970+Random.nextInt(19),
                 Random.nextInt(10)+1,Random.nextInt(25)+1)))
     }
 
-    
+    individualsMap.filter{it.key>100}.forEach{(k,v)->println("$k, ${v.dob}")}
+
+    //groupby
+    //println("Number of groups ${individualsMap.values.groupBy { it->it.dob.year }.count()}")
+    individualsMap.map { it->it.value.dob }.forEach{it->println("$it")}
+    println("Number of groups  ${individualsMap.map { it->it.value.dob }.groupBy { it->it.year }.count()}")
+
 }
