42e0b52668d962ad7dfa5897adb2fd800a5d36ff
eswaribala <parameswaribala@gmail.com>
2022-11-16 11:38:16 +0530

    updated

--- bankingapp/src/main/kotlin/com/amex/bankingapp/extensions/IndividualExtension.kt
+++ bankingapp/src/main/kotlin/com/amex/bankingapp/extensions/IndividualExtension.kt
@@ -5,6 +5,6 @@ import com.amex.bankingapp.models.Individual
 
 fun Individual.isAvailable():Boolean{
 var individualImpl= IndividualImpl()
-   return individualImpl.getIndividualById(485874)
+   return individualImpl.getIndividualById(this.accountNumber)
 
 }
