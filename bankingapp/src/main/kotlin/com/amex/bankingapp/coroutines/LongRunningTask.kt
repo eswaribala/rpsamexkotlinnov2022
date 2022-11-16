e63ad7bd23aa77414736ff070a5b6a47a409eb01
eswaribala <parameswaribala@gmail.com>
2022-11-16 14:40:10 +0530

    updated

--- bankingapp/src/main/kotlin/com/amex/bankingapp/coroutines/LongRunningTask.kt
+++ bankingapp/src/main/kotlin/com/amex/bankingapp/coroutines/LongRunningTask.kt
@@ -1,7 +1,12 @@
 package com.amex.bankingapp.coroutines
 
+import com.amex.bankingapp.dao.IndividualImpl
+import com.amex.bankingapp.models.Address
+import com.amex.bankingapp.models.Gender
+import com.amex.bankingapp.models.Individual
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import java.time.LocalDate
 
 fun main()= runBlocking {
 
@@ -17,11 +22,25 @@ fun main()= runBlocking {
 
 
 
-suspend fun fetchIndividuals(){
+suspend fun fetchIndividuals() {
     println("${Thread.currentThread().name}")
+
+    var individualImpl = IndividualImpl()
+    individualImpl.getAllIndividuals().forEach { x ->
+        println("${x.accountNo},${x.name}")
+
+    }
 }
 
 
 fun insertIndividualData(){
+
+    var individualImpl= IndividualImpl()
+
+    individualImpl.addIndividual(
+        Individual(38888,"Parameswari", Address("","","",
+            "",0),9952032862,"Parameswairbala@gmail.com","Test@123","Bala",
+            Gender.FEMALE, LocalDate.of(1970,12,2))
+    )
     println("${Thread.currentThread().name}")
 }
