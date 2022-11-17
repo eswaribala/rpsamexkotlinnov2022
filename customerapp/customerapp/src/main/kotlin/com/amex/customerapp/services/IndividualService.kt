6514b1b9b277bf3aff0626c32f20179586164ff3
eswaribala <parameswaribala@gmail.com>
2022-11-17 17:16:08 +0530

    updated

---
+++ customerapp/customerapp/src/main/kotlin/com/amex/customerapp/services/IndividualService.kt
@@ -0,0 +1,36 @@
+package com.amex.customerapp.services
+
+import com.amex.customerapp.models.Individual
+import com.amex.customerapp.repositories.IndividualRepository
+import org.springframework.stereotype.Service
+
+@Service
+class IndividualService(private val individualRepository:IndividualRepository) {
+
+    //insert
+    fun addIndividual(individual: Individual){
+
+        individualRepository.save(individual)
+    }
+
+    //select all
+    fun getAllIndividuals():List<Individual>{
+        return individualRepository.findAll()
+    }
+
+    //select individual by id
+
+    fun getIndividualById(accountNo:Long):Individual{
+        return individualRepository.findById(accountNo).orElse(null)
+    }
+
+    fun deleteIndividualById(accountNo:Long):Boolean{
+        var status:Boolean=false
+        individualRepository.deleteById(accountNo)
+        if(getIndividualById(accountNo)==null){
+            status=true
+        }
+        return status
+    }
+
+}
