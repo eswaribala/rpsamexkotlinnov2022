c8d837ed3a647b21d4b257eabb89d0a16f758856
eswaribala <parameswaribala@gmail.com>
2022-11-17 13:21:58 +0530

    updated

--- bankingapp/src/main/kotlin/com/amex/bankingapp/annotations/Validator.kt
+++ bankingapp/src/main/kotlin/com/amex/bankingapp/annotations/Validator.kt
@@ -1,4 +1,31 @@
 package com.amex.bankingapp.annotations
 
+import com.amex.bankingapp.models.Member
+
 class Validator {
+
+    fun isValid(member: Member): Boolean {
+        val fields = member::class.java.declaredFields
+        for (field in fields) {
+            field.isAccessible = true
+            for (annotation in field.annotations) {
+                val value = field.get(member)
+                if (field.isAnnotationPresent(Positive::class.java)) {
+                    val memberId = value as Long
+                    if (memberId < 0) {
+                        return false
+                    }
+                }
+                if (field.isAnnotationPresent(Alphabet::class.java)) {
+
+                    val value = field.get(member)
+
+                        val name = value as String
+                       if( !name.matches("^[a-zA-Z]*$".toRegex()))
+                          return false
+                }
+            }
+        }
+        return true
+    }
 }
--- bankingapp/src/main/kotlin/com/amex/bankingapp/utility/MemberApp.kt
+++ bankingapp/src/main/kotlin/com/amex/bankingapp/utility/MemberApp.kt
@@ -1,8 +1,10 @@
 package com.amex.bankingapp.utility
 
+import com.amex.bankingapp.annotations.Validator
 import com.amex.bankingapp.models.Member
 
 fun main(){
-    var member= Member(8465864,"Param4376743")
-
+    var member= Member(8465864,"Param")
+    var validator=Validator()
+    println(validator.isValid(member))
 }
