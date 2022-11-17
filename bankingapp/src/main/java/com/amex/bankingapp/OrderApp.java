f24ae269bc95498f7773d71f5740c30a121e9204
eswaribala <parameswaribala@gmail.com>
2022-11-17 15:05:10 +0530

    updated

--- bankingapp/src/main/java/com/amex/bankingapp/OrderApp.java
+++ bankingapp/src/main/java/com/amex/bankingapp/OrderApp.java
@@ -1,4 +1,4 @@
-package com.amex.bankingapp.utility;
+package com.amex.bankingapp;
 
 
 import com.amex.bankingapp.models.Order;
@@ -8,6 +8,6 @@ public class OrderApp {
     public static void main(String[] args){
       Order order=new Order(20);
       Order order1=new Order(1,943699);
-
+      System.out.println(order.getOrderId());
     }
 }
