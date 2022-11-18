6c877c7de968396fc7d21fc3cbd8346ef426612b
eswaribala <parameswaribala@gmail.com>
2022-11-18 13:04:42 +0530

    updated

--- bankingapp/src/test/kotlin/com/amex/bankinapp/BankingTest.kt
+++ bankingapp/src/test/kotlin/com/amex/bankinapp/BankingTest.kt
@@ -4,15 +4,19 @@ import com.amex.bankingapp.models.Member
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.function.Executable
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.CsvFileSource
+import org.junit.jupiter.params.provider.MethodSource
 import org.junit.jupiter.params.provider.ValueSource
 import java.util.*
+import java.util.stream.Stream
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 
 class BankingTest {
     private val member: Member = Member(3142,"Param")
+    private val member1:Member?=null
 
 
 
@@ -33,6 +37,17 @@ class BankingTest {
             username=null
 
         }
+
+        @JvmStatic
+        //data provider
+        fun memberDataProvider(): Stream<Arguments> {
+            return Stream.of(Arguments.of(1,"Amin","Amin"),
+                Arguments.of(2,"Amit","Amit"),
+                        Arguments.of(3,"Tulika","Tulika")
+            )
+        }
+
+
     }
 
     @Test
@@ -67,10 +82,24 @@ class BankingTest {
         member.name=name
         Assertions.assertAll(
             "Grouping more than one condition",
-            Executable { Assertions.assertTrue(member.memberId> 0) },
+            Executable { Assertions.assertNotNull(member1) },
             Executable { Assertions.assertTrue(member.name.length > 0) }
         )
 
     }
-   
+
+    @Order(4)
+    @ParameterizedTest(name = "isData")
+    @MethodSource("memberDataProvider")
+    @DisplayName("Check member id and name")
+    fun `Function name returns true from Method Source`(id:Long,name: String,expected:String) {
+        member.memberId=id
+        member.name=name
+        Assertions.assertAll(
+            "Grouping more than one condition",
+            Executable { Assertions.assertEquals(member.name,expected) },
+            Executable { Assertions.assertTrue(member.memberId > 0) }
+        )
+
+    }
 }
