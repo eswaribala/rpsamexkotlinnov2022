077bf719e8227a3b3b20898e98aa7c82105a3b51
eswaribala <parameswaribala@gmail.com>
2022-11-18 15:26:19 +0530

    updated

--- customerapp/customerapp/src/main/kotlin/com/amex/customerapp/controllers/IndividualController.kt
+++ customerapp/customerapp/src/main/kotlin/com/amex/customerapp/controllers/IndividualController.kt
@@ -1,8 +1,11 @@
 package com.amex.customerapp.controllers
 
+import com.amex.customerapp.dtos.DataWrapper
 import com.amex.customerapp.models.Individual
 import com.amex.customerapp.services.IndividualService
 import com.example.api.controller.model.IndividualDto
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 //import io.swagger.annotations.Api
 import org.springframework.web.bind.annotation.*
 
@@ -13,14 +16,23 @@ class IndividualController(private val individualService: IndividualService) {
 
     @GetMapping("/v1.0")
     fun getAllIndividuals(): List<IndividualDto> {
+
         return individualService.getAllIndividuals().map {
             IndividualDto.from(it)
         }
+
+
     }
 
     @GetMapping("/v1.0/{id}")
-    fun getIndividualsById(@PathVariable("id") IndividualId: Long): IndividualDto {
-        return IndividualDto.from(individualService.getIndividualById(IndividualId))
+    fun getIndividualsById(@PathVariable("id") IndividualId: Long): ResponseEntity<DataWrapper<Any>> {
+       // return IndividualDto.from(individualService.getIndividualById(IndividualId))
+
+        var individual:Individual=individualService.getIndividualById(IndividualId);
+        if(individual!=null)
+            return ResponseEntity.status(HttpStatus.OK).body(DataWrapper(individual));
+        else
+            return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(DataWrapper("Customer Info not correct"));
     }
 
     @PostMapping("/v1.0")
---
+++ customerapp/customerapp/src/main/kotlin/com/amex/customerapp/dtos/DataWrapper.kt
@@ -0,0 +1,21 @@
+package com.amex.customerapp.dtos
+
+class DataWrapper<T> {
+    private var _message: String? = null
+    private var _payload: T? = null
+    var msg:String?=null
+    var pload:T?=null
+    constructor(message: String?) : super() {
+        this._message = message
+        msg=_message
+    }
+
+    constructor(payload: T) : super() {
+        this._payload = payload
+         pload=_payload
+    }
+
+
+
+
+}
