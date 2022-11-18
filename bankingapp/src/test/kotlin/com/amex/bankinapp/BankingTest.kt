a97f556464b680c0066f5a5807cef685b739761d
eswaribala <parameswaribala@gmail.com>
2022-11-18 12:07:33 +0530

    updated

--- bankingapp/src/test/kotlin/com/amex/bankinapp/BankingTest.kt
+++ bankingapp/src/test/kotlin/com/amex/bankinapp/BankingTest.kt
@@ -2,6 +2,8 @@ package com.amex.bankinapp
 
 import com.amex.bankingapp.models.Member
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
@@ -15,17 +17,21 @@ import kotlin.test.assertTrue
 class BankingTest {
     private val member: Member = Member(3142,"Param")
 
+    @Order(2)
     @Test
     @DisplayName("Name Should be Equal to Expected")
+    @RepeatedTest(3)
     fun testMember() {
         val expected = "Param"
         assertEquals(expected, member.name)
 
 
     }
-    @DisplayName("Name Length Should be > 0")
+    @Order(1)
+
     @ParameterizedTest(name = "isMember")
     @ValueSource(strings = ["Param", "Vignesh", "Bala", ""])
+    @DisplayName("Name Length Should be > 0")
     fun `Function isMember returns true for Name`(name: String) {
         member.name=name
         assertTrue(member.name.length>0)
